054b0889d199476648ce3edc4c00e19b43413a9e
David Atkins <atki1080@gmail.com>
2020-03-03 15:22:30 -0700

    Add test for incomplete discount

--- spec/features/merchant/discounts/new_spec.rb
+++ spec/features/merchant/discounts/new_spec.rb
@@ -21,4 +21,26 @@ RSpec.describe "As a merchant employee" do
     expect(page).to have_content('15')
     expect(page).to have_content('30')
   end
+
+  it "I get an error if I leave a field blank" do
+    user = create(:merchant_user)
+    discount = create(:random_discount, percent_off: 5, requirement: 5)
+    user.merchant.discounts << discount
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit merchant_discounts_path
+    click_link("Add a New Bulk Discount")
+
+    fill_in :discount_percent_off, with: 15
+
+    click_button("Create Discount")
+
+    expect(page).to have_content("Requirement can't be blank")
+    
+    user.reload
+    visit merchant_discounts_path
+
+    expect(page).to_not have_content('15')
+  end
 end
