9c186ef3a2cf90ad04aef7caa15ca5d817591d76
David Atkins <atki1080@gmail.com>
2020-03-03 13:45:14 -0700

    Add new action

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -2,4 +2,13 @@ class Merchant::DiscountsController < Merchant::BaseController
   def index
     @discounts = current_user.merchant.discounts
   end
+
+  def new
+    @discount = Discount.new
+  end
+
+  private
+    def discount_params
+      params.require(:discount).permit(:percent_off, :requirement)
+    end
 end
