b20425fa65cc217220b67784ee01523a55967257
David Atkins <atki1080@gmail.com>
2020-03-04 10:39:21 -0700

    Add numericality validation

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,6 +1,8 @@
 class Discount < ApplicationRecord
   belongs_to :merchant
   validates_presence_of :percent_off, :requirement
+  validates_numericality_of :percent_off, greater_than: 0, less_than_or_equal_to: 100
+  validates_numericality_of :requirement, greater_than: 0
   enum status: %w(inactive active)
 
   def self.active
